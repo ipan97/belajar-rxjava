3543a0dc17806bc124229eea673f7f7e49e73efd
Ipan Taupik Rahman <ipan.rahman@wgs.co.id>
2017-12-26 02:02:49 +0700

    Add dependencies logger slf4j and rxjava2

--- src/main/java/com/github/ipan97/App.java
+++ src/main/java/com/github/ipan97/App.java
@@ -1,5 +1,8 @@
 package com.github.ipan97;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 /**
  * Created by Ipan Taupik Rahman.
  * Date: 26/12/17
@@ -9,7 +12,11 @@ package com.github.ipan97;
  */
 public class App {
 
+    private static final Logger LOGGER = LoggerFactory.getLogger(App.class);
+
     public static void main(String[] args) {
+
+        LOGGER.info("Start App...");
 
     }
 }
