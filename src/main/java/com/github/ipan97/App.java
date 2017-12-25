80ce3973b9452111b1fb0fcf3649d1a37f01ebe1
Ipan Taupik Rahman <ipan.rahman@wgs.co.id>
2017-12-26 01:07:37 +0700

    initial commit

---
+++ src/main/java/com/github/ipan97/App.java
@@ -0,0 +1,11 @@
+package com.github.ipan97;
+
+/**
+ * Created by Ipan Taupik Rahman.
+ * Date: 26/12/17
+ * Time: 1:06
+ * Project belajar-rxjava
+ * Package PACKAGE_NAME
+ */
+public class App {
+}
